a42ce151c3ab9c598f759b3c63aa4ad738e9121c
Antônio Assis de Sousa Neto <netosouzaifce@gmail.com>
2022-12-05 15:49:15 -0300

    refactor: exclude component v-heading and add normal className to stylize the elements

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,43 +1,62 @@
 <script lang="ts">
     import { defineComponent } from 'vue';
-import HeadingVue from './UI/Heading.vue';
     import ButtonVue from '/@/components/UI/Button.vue';
 
     export default defineComponent({
         name: 'Main',
         components: {
             'v-button': ButtonVue,
-            'v-heading': HeadingVue,
         }
     })
 </script>
 
 <template>
     <main>
-        <section>
-            <v-heading>
+        <section class="main-text-group">
+            <span class="heading5" role="h5">
                 SO, YOU WANT TO TRAVEL TO
-            </v-heading>
-            <span>
-                SPACE
             </span>
-            <p>
+            <span class="heading1" role="h1">
+                SPACE 
+            </span>
+            <span class="body-text" role="p">
                 Let’s face it; if you want to go to space, 
                 you might as well genuinely go to outer space 
                 and not hover kind of on the edge of it. Well sit back, 
                 and relax because we’ll give you a truly out of this world experience!
-            </p>
+            </span>
         </section>
 
-        <section>
+        <section class="main-btn-section">
             <v-button></v-button>
         </section>
     </main>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
     main {
+        width: 80%;
+        margin: auto;
+
         display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+        
+        color: white;
+        
+        .main-text-group {
+            width: 40%;
+            display: flex;
+            flex-direction: column;
+            text-align: start;
+
+            &:first-child(){
+                margin-bottom: 24px;
+            }
+            &:child(2n){
+                margin-bottom: 24px;
+            }
+        }
     }
 </style>
