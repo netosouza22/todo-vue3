2cf75474c3bd7a71c930b2a3984faa18b797c12a
Antônio Assis de Sousa Neto <netosouzaifce@gmail.com>
2022-12-07 21:13:09 -0300

    feat: add config to permit that some scss files be used globally

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
+import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "src/assets/styles/_variables.scss";
+        @import "src/assets/styles/_typography.scss"; 
+        @import "src/assets/styles/_breakpoints.scss";`
+      }
+    }
+  },
+  resolve: {
+    alias: [
+      {
+        find: '/@',
+        replacement: path.resolve(__dirname, './src')
+      },
+    ]
+  },
   plugins: [vue(), eslintPlugin()],
 });
