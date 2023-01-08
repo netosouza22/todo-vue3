ba4cf5175a91f7685f82551e95b60d2aa20781db
Antônio Assis de Sousa Neto <netosouzaifce@gmail.com>
2023-01-08 14:23:29 -0300

    feat: create the components of menu page

---
+++ src/components/crew/CrewInfo.vue
@@ -0,0 +1,62 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import NavCrew from './NavCrew.vue';
+
+export default defineComponent({
+    name: 'CrewInfo',
+    components: {
+        NavCrew,
+    },
+    data() {
+        return { crews: [] };
+    },
+});
+</script>
+
+<template>
+    <section class="crew__info">
+        <main>
+            <h4 class="heading4">COMMANDER</h4>
+            <h3 class="heading3">DOUGLAS HURLEY</h3>
+            <p class="body-text">
+                Douglas Gerald Hurley is an American engineer, former Marine
+                Corps pilot and former NASA astronaut. He launched into space
+                for the third time as commander of Crew Dragon Demo-2.
+            </p>
+        </main>
+        <footer>
+            <NavCrew></NavCrew>
+        </footer>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.crew__info {
+    width: 55%;
+
+    main {
+        margin: 50px 0px;
+        align-items: flex-start;
+
+        h4 {
+            margin-bottom: 1.2rem;
+        }
+        h3 {
+            margin-bottom: 1.8rem;
+        }
+
+        p {
+            width: 80%;
+        }
+    }
+
+    footer {
+        align-items: flex-end;
+    }
+
+    @include lg {
+        text-align: start;
+    }
+}
+</style>
---
+++ src/components/crew/NavCrew.vue
@@ -0,0 +1,70 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'NavCrew',
+    data() {
+        return {};
+    },
+    methods: {},
+});
+</script>
+
+<template>
+    <header>
+        <div class="nav__menu">
+            <ul>
+                <li className="nav__item nav__text nav__item--active"></li>
+                <li className="nav__item nav__text"></li>
+                <li className="nav__item nav__text"></li>
+                <li className="nav__item nav__text"></li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.hide {
+    display: none;
+}
+header {
+    width: 100%;
+    position: relative;
+
+    .nav__menu {
+        width: 80%;
+        margin: auto;
+        position: relative;
+
+        ul {
+            display: flex;
+            margin: auto;
+            gap: 1.6rem;
+
+            .nav__item {
+                cursor: pointer;
+                width: 10px;
+                height: 10px;
+                background-color: $light;
+                border-radius: 50%;
+                opacity: 17%;
+
+                position: relative;
+                letter-spacing: 2.7px;
+            }
+
+            .nav__item:hover {
+                opacity: 50%;
+            }
+
+            .nav__item--active {
+                opacity: 100%;
+            }
+        }
+
+        @include lg {
+            margin: 0;
+        }
+    }
+}
+</style>
