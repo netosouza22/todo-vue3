f42807b9fc91fda931294cb093de1f5fed924725
Antônio Assis de Sousa Neto <netosouzaifce@gmail.com>
2023-01-07 21:08:01 -0300

    feat: add button in new folder

---
+++ src/components/base/Button.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Button',
+});
+</script>
+
+<template>
+  <button class="btn">EXPLORE</button>
+</template>
+
+<style lang="scss">
+.btn {
+  border: 1px solid #000;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background: white;
+  cursor: pointer;
+
+  border: none;
+  font-size: 1.5rem;
+
+  letter-spacing: 0.125rem;
+  color: #0b0d17;
+}
+
+.btn:hover {
+  animation: pulse 1.8s infinite;
+  transition: 1s ease infinite;
+}
+
+@keyframes pulse {
+  0% {
+    box-shadow: 0 0 0 0 rgba(151, 151, 151, 0.4);
+  }
+
+  50% {
+    box-shadow: 0 0 0 50px rgba(151, 151, 151, 0.4);
+  }
+
+  100% {
+    box-shadow: 0 0 0 0px rgba(151, 151, 151, 0.4);
+  }
+}
+</style>
