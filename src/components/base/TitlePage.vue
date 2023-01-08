9c6ed7de992144de1f23759c3d92d5dd15e9db55
Antônio Assis de Sousa Neto <netosouzaifce@gmail.com>
2023-01-07 21:20:50 -0300

    feat: title of the page

---
+++ src/components/base/TitlePage.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'TitlePage',
+    props: {
+        number: { type: Number, required: true },
+        text: { type: String, required: true },
+    },
+    data() {
+        return {};
+    },
+    methods: {},
+});
+</script>
+
+<template>
+    <section>
+        <h2 class="heading5">
+            <span class="title__marker">{{ number }}</span
+            >{{ text }}
+        </h2>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+    width: 100%;
+    text-align: center;
+    h2 {
+        font-weight: 400;
+        .title__marker {
+            font-weight: bold;
+            mix-blend-mode: normal;
+            opacity: 0.25;
+            margin-right: 1.5rem;
+        }
+    }
+
+    @include md {
+        text-align: start;
+    }
+}
+</style>
